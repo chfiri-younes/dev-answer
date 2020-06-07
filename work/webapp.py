1276f07fe67ee60f0fb083cd92d812f2017e6c8c
chfiri-younes <chfiri.y321@gmail.com>
2020-06-07 15:26:33 +0100

    v0.2

--- work/webapp.py
+++ work/webapp.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, session, request, redirect, url_for
+
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def accueil():
     return render_template('accueil.html')
 
 @app.route('/about')
@@ -17,6 +19,20 @@ def support ():
 @app.route('/rules')
 def rules ():
     return render_template('rules.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Username of Password. Please try again.'
+        else:
+            return redirect(url_for('accueil'))
+    return render_template('_login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
